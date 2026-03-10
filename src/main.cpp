5af206291a0c8348c2b4fe296662d28943915f05
Nishanth <139257961+nishanthchandr4@users.noreply.github.com>
2026-03-10 17:27:18 -0400

    milestone 2

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,12 @@ FEHMotor right_motor(FEHMotor:: Motor0, 9.0);
 FEHMotor left_motor(FEHMotor:: Motor1, 9.0);
 
 //declarations for CdS Cell
-AnalogInputPin cds_cell(FEHIO::Pin0);
+AnalogInputPin cds_cell(FEHIO::Pin3);
 
 //declarations for optosensors
-AnalogInputPin left_opto();
-AnalogInputPin middle_opto();
-AnalogInputPin right_opto();
+AnalogInputPin left_opto(FEHIO::Pin2);
+AnalogInputPin middle_opto(FEHIO::Pin1);
+AnalogInputPin right_opto(FEHIO::Pin0);
 
 
 //input a negative percent if you want to move backwards
@@ -197,77 +197,4 @@ void ERCMain()
  
     //test 2 going up and down the ramp comment this out when doing test 1
     //test2();
-
-    //move this to a different file adin everything bellow this comment
-    //exploration 3 code
-
-    // Declare variables.
-    // For touchscreen functionality.
-    int touch_x, touch_y;
-    // Loop n through points to record necessary positions.
-    int n;
-    char points[4] = {'A', 'B', 'C', 'D'};
-
-    // Disable RCS position rate limiting to allow for more
-    // frequent position updates (testing only, won't work during competition)
-    RCS.DisableRateLimit();
-
-    // Call this function to initialize the RCS to a course.
-    // For Exploration 3, please use the key provided below.
-    //     This matches the AruCo code that is provided for this exploration.
-    // If your team wishes to use RCS's positioning system going forward,
-    //     please use the 8 digit code on your team's page on the store website.
-    RCS.InitializeTouchMenu("D3TESTING");
-
-    // Open SD file for writing
-    FEHFile *fptr = SD.FOpen("test.txt", "w");
-
-    // Wait for touchscreen to be pressed and released
-    LCD.WriteLine("Press Screen to Start");
-    while (!LCD.Touch(&touch_x, &touch_y));
-    while (LCD.Touch(&touch_x, &touch_y));
-
-    // Clear screen
-    LCD.Clear();
-
-    // Step through each path point (A,B,C,D) to record position and heading
-    for (n = 0; n <= 3; n++)
-    {
-        LCD.Clear();
-        LCD.WriteRC("Touch to set point ", 0, 0);
-        LCD.WriteRC(points[n], 0, 20);
-
-        RCSPose *pose;
-
-        // Loop until touchscreen is pressed, continuously requesting 
-        // RCS position data and writing it to the LCD screen
-        while (!LCD.Touch(&touch_x, &touch_y))
-        {
-            // Request position data from RCS
-            pose = RCS.RequestPosition();
-
-            // Clear previous position data from LCD
-            LCD.SetFontColor(BLACK);
-            LCD.FillRectangle(130, 20, 100, 60);
-
-            LCD.SetFontColor(WHITE);
-            LCD.WriteRC("X Position:", 2, 0);
-            LCD.WriteRC("Y Position:", 3, 0);
-            LCD.WriteRC("Heading:", 4, 0);
-
-            // Write the RCS data to the LCD screen
-            LCD.WriteRC(pose->x, 2, 12);
-            LCD.WriteRC(pose->y, 3, 12);
-            LCD.WriteRC(pose->heading, 4, 12);
-        }
-
-        // Print RCS data for this path point to file
-        SD.FPrintf(fptr, "%f %f\n", pose->x, pose->y);
-
-        // Wait for touchscreen to be released
-        while (!LCD.Touch(&touch_x, &touch_y));
-    }
-
-    // Close SD file
-    SD.FClose(fptr);
 }
